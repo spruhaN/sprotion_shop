e35072cb759e58fbef9b719ebffd82d289d9bcaf
spruha <spruhanayak2004@gmail.com>
2024-04-08 01:22:49 -0700

    persistance - start err

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,14 +10,15 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
+    green_pots = 0
     # get available green pots from inventory
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
-    green_pots = result.first().num_green_potions
+        green_pots = result.first().num_green_potions
 
-    # if no green pots then []
-    if green_pots == 0:
-        return []
+        # if no green pots then []
+        if green_pots == 0:
+            return []
     
     # else return how many are available
     return [
